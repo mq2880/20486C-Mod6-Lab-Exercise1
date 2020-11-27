b0e017186a31670b6bcfa38575139c3e83ee0214
Javier Moreno <mq2880@gmail.com>
2020-11-27 17:41:10 +0100

    Commit

---
+++ ButterfliesShop/Models/Butterfly.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace ButterfliesShop.Models
+{
+    public class Butterfly
+    {
+        public int Id { get; set; }
+        public string CommonName { get; set; }
+        public Family? ButterflyFamily { get; set; }
+        public int? Quantity { get; set; }
+        public string Characteristics { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IFormFile PhotoAvatar { get; set; }
+        public string ImageName { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string ImageMimeType { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 20486C-Mod6-Lab-Exercise1
+# Module 6: Developing Models
+
+## Lab: Developing Models
+
+1. **Nombres y apellidos:** Francisco Javier Moreno Quevedo
+2. **Fecha:** 27/11/2020
+3. **Resumen del Ejercicio:** Añadir los componentes necesarios para hacer funcional la tienda de mariposas 1/3 
+4. **Dificultad o problemas presentados y como se resolvieron:** Ninguna
+
+
+
+- Ejercicio 1: Adding a Model
+
+  - Crear un dos clases **Butterfly y IndexViewModel** añadirle las propiedades
+  - En la pagina principal añadir estos modelos para su visualización
+  - Añadir en su controlador las acciones oportunas
